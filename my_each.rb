72f8d4b5b32dd0458780471b8139e4c3e262ea31
Oksana Nichols <oksanahnichols@yahoo.com>
2019-11-18 06:56:41 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
 def my_each # put argument(s) here
-  # code here
+  require_relative 'spec_helper'
+require 'pry'
+
+describe "my_each" do
+  file = File.read('./my_each.rb')
+
+  it "does not call on .each" do
+    expect(file).to_not include(".each")
+  end
 end
