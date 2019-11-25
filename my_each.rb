7ea9813e0167b1ea9dd0f413cb24f4e4a846b718
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-11-25 22:22:15 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
+def my_each(words) # put argument(s) here
+words.length.times
+   # words do |i|
+     # i
+    # end
+  # while i == 4
+ # yield(words)
+ # end
   # code here
 end
