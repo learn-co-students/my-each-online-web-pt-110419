73b82dceb607a601a97baddf58299aa35b5f4d67
Mintra Phophaijit <bluembc@gmail.com>
2019-11-26 18:17:47 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,25 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i = 0
+ while i <array.legnth
+ yeild array [i]
+ i = i + 1
 end
+end
+[1, 2, 3, 4]:Array
+
+ my_each(words) do |word|
+         puts word
+       end
+       
+ my_each(words) do |item|
+         expect(item).to equal(words[counter])
+         counter+=1
+       end
+       
+       it "calls on while" do
+    expect(file).to include("while")
+  end
+  
+   it "iterates over each element" do
+    words = ['hi', 'hello', 'bye', 'goodbye']
+
