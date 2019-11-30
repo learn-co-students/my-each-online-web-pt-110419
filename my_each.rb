6d6b52184f48e440c7c40cd804fff20182142776
kenneth gil <astronori@gmail.com>
2019-11-30 16:25:10 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,21 @@
-def my_each # put argument(s) here
-  # code here
+list = ["cab", "gal", "pop"]
+def my_each(array)
+  i = 0
+ while i < array.length
+ yield array[i]
+ i = i + 1
+ end
 end
+my_each(list) {"I feel like #{i} is hard to learn"}
+
+def hello(array)
+  i = 0
+collection = []
+while i < array.length
+collection << yield(array[i])
+i += 1
+end
+collection
+end
+
+hello(list) {|i| "Hello #{i} how is everything?"}
